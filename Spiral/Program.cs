660569bf56dbf9a652804411d1fde782fce40ce9
gaynor-landmark <g.l.landmark@gmail.com>
2016-11-05 15:49:50 +1300

    removed all test code to clear the program file

--- Spiral/Program.cs
+++ Spiral/Program.cs
@@ -10,14 +10,7 @@ namespace Spiral
     {
         static void Main(string[] args)
         {
-            int[] testArr = new int[10];
-            for (int i = 0; i < testArr.Length; i++) {
-                testArr[i] = 2 * i;
-            }
-            Console.WriteLine(testArr[7]);
-
-            int x = Int32.Parse(Console.ReadLine());
-            Console.WriteLine(testArr[x]);
+           
             Console.ReadLine();
         }
     }
